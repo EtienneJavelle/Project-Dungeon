8f8be30b0c08dbfc725ee8c53046e2366a48f5a2
etiennejavelle <etienne.javelle@lacatholille.fr>
2021-01-06 11:08:29 +0100

    Sln

---
+++ Project-Dungeon/Dungeon/Dungeon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Dungeon
+{
+    class Dungeon
+    {
+        #region Variables
+        public char[,] DungeonMap = new char[5, 5]
+            {
+                {'-','M','I','-','B' },
+                {'-','I','-','M','-' },
+                {'S','M','B','M','I' },
+                {'I','-','M','E','M' },
+                {'I','-','M','-','B' }
+            };
+        #endregion
+
+        #region Construct
+        public Dungeon(char[,] dungeonMap)
+        {
+            DungeonMap = dungeonMap;
+        }
+        #endregion
+
+        #region Overrides
+        public override string ToString()
+        {
+            string tmp = "";
+            for (int x = 0; x < DungeonMap.GetLength(0); x++)
+            {
+                for (int y = 0; y < DungeonMap.GetLength(1); y++)
+                {
+                    tmp += DungeonMap[x, y];
+                }
+                tmp += "\n";
+            }
+            return tmp;
+        }
+        #endregion
+        
+        #region Methods
+        #endregion
+
+        #region Fields
+        #endregion
+    }
+}
--- Project-Dungeon/Program.cs
+++ Project-Dungeon/Program.cs
@@ -6,7 +6,32 @@ namespace Project_Dungeon
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char[,] Level1 = new char[5, 5]
+            {
+                {'-','M','I','-','B' },
+                {'-','I','-','M','-' },
+                {'S','M','B','M','I' },
+                {'I','-','M','E','M' },
+                {'I','-','M','-','B' }
+            };
+
+            int[,]playerPos = new int[1, 2] { { 2, 0 } };
+
+            Dungeon dungeon = new Dungeon(Level1);
+
+            string input;
+            do
+            {
+                dungeon.DungeonMap[playerPos[0, 0], playerPos[0, 1]] = 'P';
+                Console.Write(dungeon);
+                input = Console.ReadLine().ToLower();
+                if(input == "d") playerPos[0,1]++;
+                if(input == "a") playerPos[0,1]--;
+                if(input == "s") playerPos[0,0]++;
+                if(input == "w") playerPos[0,0]--;
+
+                Console.Clear();
+            } while (input != "quit");
         }
     }
 }
