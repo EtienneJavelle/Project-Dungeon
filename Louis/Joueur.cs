f8925dd6cc420b7a9636addacf03056c1ea83141
LDPIIID <louisduprat59@gmail.com>
2021-01-18 14:33:40 +0100

    Les perso (Joueur ennemi)

---
+++ Louis/Joueur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P_Dungeon
+{
+    class Joueur: Personnage
+    {
+        public int Or { get; set; }
+        public int EXP { get; set; }
+        public int LVL { get; set; }
+
+        public Joueur(string nom) : base(nom)
+        {
+            this.EXP = 0;
+        }
+
+        public void LevelUP()
+        {
+            this.LVL++;
+        }
+
+    }
+}
---
+++ Louis/Personnage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P_Dungeon
+{
+    class Personnage
+    {
+        #region Variables
+
+        public Personnage (int Vie, int Force)
+        {
+            this.Vie = Vie;
+            this.Force = Force;
+            this.Armure = 0;
+            this.Arme = 0;
+            this.Inventaire = new List<Item>;
+            
+        }
+
+        int x;
+        int y;
+
+        #endregion
+
+        #region Construct
+
+        public int Vie { get; }
+        public int Force { get; }
+        public int Armure { get; }
+        public int Arme { get; }
+        public List<Item> Inventaire { get; set; }
+        
+
+        #endregion
+
+        #region Overrides
+        #endregion
+
+        #region Methods
+
+        public void AjouterItem(Item item)
+        {
+            this.Inventaire.Add(item);
+        }
+        public void SupprimerItem(Item item)
+        {
+            this.Inventaire.Remove(item);
+        }
+        
+        public void Attaquer()
+        {
+            Console.WriteLine("Attaque !");
+        }
+
+        #endregion
+
+        #region Fields
+        #endregion
+
+    }
+
+   
+
+}
