8318acc088d70c6028681481ac9a223f61e30382
etiennejavelle <etienne.javelle@lacatholille.fr>
2021-01-18 16:39:25 +0100

    dfs

--- Program.cs
+++ Program.cs
@@ -28,9 +28,8 @@ namespace Project_Dungeon
             };
             #endregion
 
-            //int[] playerPos = new int[2] { 2, 0 };
-            Vector2 playerPos = new Vector2(2, 0);
-            Vector2 lastPlayerPos = new Vector2( 2, 0 );
+            Player player = new Player();
+            player.Position = new Vector2Int(2, 0);
 
             Dungeon dungeon = new Dungeon(DungeonFloors);
 
@@ -39,38 +38,31 @@ namespace Project_Dungeon
             do
             {
                 Console.Clear();
-                dungeon.DrawMap(playerPos);
+                dungeon.DrawMap(player.Position);
                 Console.WriteLine("Floor " + dungeon.Floor);
 
-                input = Inputs(playerPos, dungeon);
+                input = Inputs(player, dungeon);
 
             } while (input.Key != ConsoleKey.Escape);
         }
 
-        private static ConsoleKeyInfo Inputs(Vector2 playerPos, Dungeon dungeon)
+        private static ConsoleKeyInfo Inputs(Player player, Dungeon dungeon)
         {
             ConsoleKeyInfo input = Console.ReadKey();
-            Vector2 dir = Vector2.zero;
-            if (input.Key == ConsoleKey.D || input.Key == ConsoleKey.RightArrow) dir=Vector2.right;
-            else if (input.Key == ConsoleKey.A || input.Key == ConsoleKey.LeftArrow) dir=Vector2.left;
-            else if (input.Key == ConsoleKey.S || input.Key == ConsoleKey.DownArrow) dir=Vector2.down;
-            else if (input.Key == ConsoleKey.W || input.Key == ConsoleKey.UpArrow) dir =Vector2.up;
+            Vector2Int dir = Vector2Int.zero;
+            if (input.Key == ConsoleKey.D || input.Key == ConsoleKey.RightArrow) dir = Vector2Int.right;
+            else if (input.Key == ConsoleKey.A || input.Key == ConsoleKey.LeftArrow) dir = Vector2Int.left;
+            else if (input.Key == ConsoleKey.S || input.Key == ConsoleKey.DownArrow) dir = Vector2Int.down;
+            else if (input.Key == ConsoleKey.W || input.Key == ConsoleKey.UpArrow) dir = Vector2Int.up;
             else if (input.Key == ConsoleKey.E) dungeon.Floor++;
             else if (input.Key == ConsoleKey.Q) dungeon.Floor--;
 
-            Move(playerPos, dir, Vector2.right);
+            player.Move(dir);
 
-            playerPos.x = Math.Clamp(playerPos.x, 0, 4);
-            playerPos.y = Math.Clamp(playerPos.y, 0, 4);
+            player.Position.x = Math.Clamp(player.Position.x, 0, 4);
+            player.Position.y = Math.Clamp(player.Position.y, 0, 4);
             dungeon.Floor = Math.Clamp(dungeon.Floor, 0, 1);
             return input;
-        }
-
-        static void Move(Vector2 playerPos, Vector2 dir, Vector2 lastPlayerPos)
-        {
-            lastPlayerPos = playerPos;
-            playerPos += dir;
-            playerPos += Vector2.right;
         }
     }
 }
